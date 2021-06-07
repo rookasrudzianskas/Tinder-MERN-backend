301750b2a553800cc80edd62854667c8b686b309
Rokas <rookas.rudzenskas@gmail.com>
2021-06-07 11:31:28 +0300

    Rokas

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import Cors from "cors";
-
+import Cards from "./dbCards";
 
 // app config
 const app = express();
@@ -27,7 +27,30 @@ mongoose.connect(connection_url, {
 // api endpoints
 
 app.get('/', (req, res) => res.status(200).send("hello world 🔥"));
+app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body;
 
+    Cards.create(dbCard, (err, data) => {
+        if(err) {
+        res.status(500).send(err);
+    } else {
+        res.status(200).send(data);
+        }
+    })
+});
+
+
+app.get('/tinder/card', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.find( (err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+});
 // listener
 app.listen(port, () => console.log(`listening on localhost ${port}`));
 
